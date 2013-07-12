ab1bbcc3a87096b5a53c5f01eb473d8c3e726440
Parker Moore <parkrmoore@gmail.com>
2013-07-12 21:15:27 +0000

    Add engine module.

---
+++ engine.py
@@ -0,0 +1,11 @@
+from sqlalchemy import create_engine
+import toml
+
+def build_engine():
+  conf = load_configs()
+  return create_engine('mysql+pymysql://%s:%s@%s/%s' % (conf["username"],
+    conf["password"], conf["host"], conf["database"]))
+
+def load_configs(config_file="config/database.toml"):
+  with open(config_file) as f:
+    return toml.loads(f.read())
